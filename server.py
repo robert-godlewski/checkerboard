f20e0160b53cbcfc4c943e554a789f97bee146b3
Robert Godlewski <robtechg5@gmail.com>
2022-01-23 16:24:46 -0800

    Added in a row counter.

--- server.py
+++ server.py
@@ -4,7 +4,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def regular_board(): return render_template("index.html")
+def regular_board(): return render_template("index.html", rows=8)
+
+@app.route('/<x>')
+def board_x_rows(x):
+    max = int(x)
+    return render_template("index.html", rows=max)
 
 
 if __name__=="__main__": app.run(debug=True)
